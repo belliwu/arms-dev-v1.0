351650026385afdab2e13377c600ca2ed19068a9
ZONES <belliwu@iii.org.tw>
2018-08-09 17:25:35 +0800

    Modify Home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import Home from '@/views/layout/Home'
-import TestPage from '@/components/TestPage'
+import TestPage from "@/components/TestPage";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/test',
-      name: 'TestPage',
+      path: "/",
+      name: "TestPage",
       component: TestPage
     }
   ]
-})
+});
